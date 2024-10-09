9f071fb0b6c3cb1c49caa4bd3bedd6e6ff93a560
T-DENNER <thiagoleite1107@gmail.com>
2024-10-09 03:24:10 -0300

    feat:Primeira versao

---
+++ exemplo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a;
+    int b;
+    int c;
+
+    cout << "Exemplo inicial de aplicativo" << endl;
+    cout << "Digite um numero: ";
+    cin >> a;
+    cout << "Digite outro numero: ";
+    cin >> b;
+    cout << "Outro: ";
+    cin >> c;
+    cout << "A soma dos numeros e: " << a + b + c << endl;
+
+    system("pause");
+    return 0;
+    }
